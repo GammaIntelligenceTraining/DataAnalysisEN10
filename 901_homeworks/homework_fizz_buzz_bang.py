cc54a766828c1ac112f1af68f68a588930c25946
Roman Kutselepa <roman.kutselepa@gmail.com>
2026-05-13 16:48:52 +0300

    latest

---
+++ 901_homeworks/homework_fizz_buzz_bang.py
@@ -0,0 +1,12 @@
+# Fizz Buzz Bang
+# Create a function that accespts two numbers
+# start and end
+# Inside function run a cycle for numeric row between theese numbers
+# If number is divisible by 3 without a remainder print number and FIZZ
+# If number is divisible by 5 without a remainder print number and BUZZ
+# If number is divisible by 7 without a remainder print number and BANG
+# If divisible by combination, in example by 3 and 7 -> print combination FIZZBANG
+# or divisible by 5 and 7 -> print number and combination BUZZBANG
+# or divisible by 3, 5 and 7 -> print number and combination FIZZBUZZBANG
+# Otherwise skip to next number
+# All combinations must be covered
